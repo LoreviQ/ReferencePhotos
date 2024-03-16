59de782944afec95223aa8630143b317b8dedae9
LoreviQ <lorevi@protonmail.com>
2024-03-16 19:37:06 +0000

    Added internal package to obtain screen resolution

---
+++ internal/resolution/screenresolution_test.go
@@ -0,0 +1,8 @@
+package resolution
+
+import "fmt"
+
+func ExampleGetPrimary() {
+	fmt.Println(GetPrimary().String())
+	// Output: 2560x1440
+}
--- main.go
+++ main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"gioui.org/app"
+	"gioui.org/unit"
+	"github.com/LoreviQ/ReferencePhotos/internal/resolution"
 )
 
 func main() {
 	go func() {
+		// Define Resolution
+		width, height := 400, 600
+		res := resolution.GetPrimary()
+		if res != nil {
+			width, height = res.Width, res.Height
+		}
+
 		// create new window
-		w := app.NewWindow()
+		w := app.NewWindow(
+			app.Title("Reference Photos"),
+			app.Size(unit.Dp(width), unit.Dp(height)),
+		)
 
 		// listen for events in the window
 		for {
