444d3fa479ea2ecf8e743f0a6c0e4441d6af0fa0
LoreviQ <lorevi@protonmail.com>
2024-03-25 19:44:18 +0000

    Added test to produce windows specific system sounds

--- go.mod
+++ go.mod
@@ -9,21 +9,26 @@ require (
 	gioui.org/shader v1.0.8 // indirect
 	github.com/akavel/rsrc v0.10.2 // indirect
 	github.com/dchest/jsmin v0.0.0-20220218165748-59f39799265f // indirect
+	github.com/gen2brain/beeep v0.0.0-20240112042604-c7bb2cd88fea // indirect
 	github.com/gen2brain/svg v0.1.0 // indirect
 	github.com/go-text/typesetting v0.0.0-20230803102845-24e03d8b5372 // indirect
+	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/josephspurrier/goversioninfo v1.4.0 // indirect
 	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
 	github.com/ncruces/zenity v0.10.12 // indirect
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
 	github.com/randall77/makefat v0.0.0-20210315173500-7ddd0e42c844 // indirect
 	github.com/srwiley/oksvg v0.0.0-20221011165216-be6e8873101c // indirect
 	github.com/srwiley/rasterx v0.0.0-20220730225603-2ab79fcdd4ef // indirect
+	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
 	golang.org/x/exp v0.0.0-20221012211006-4de253d81b95 // indirect
 	golang.org/x/exp/shiny v0.0.0-20220827204233-334a2380cb91 // indirect
 	golang.org/x/image v0.15.0 // indirect
 	golang.org/x/mod v0.8.0 // indirect
 	golang.org/x/net v0.6.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/tools v0.6.0 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
--- progress.go
+++ progress.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"syscall"
 	"time"
 
 	"gioui.org/app"
+	"github.com/gen2brain/beeep"
 )
 
 type progressBar struct {
@@ -21,7 +24,7 @@ func getProgressIncrementer(increment string) chan float32 {
 		"1m":  0.00016666666,
 		"2m":  0.00008333333,
 		"5m":  0.00003333333,
-		"10m": 0.01,
+		"10m": 0.002,
 		//"10m": 0.00001666666,
 	}
 	progressIncrementer := make(chan float32)
@@ -50,16 +53,37 @@ func incrementProgress(window *app.Window, progressIncrementer chan float32, exi
 }
 
 func playSound(p float32) {
+	var err error
 	if localState.progressBar.progress > 1-p*300 && localState.progressBar.sounds == 0 {
 		fmt.Println("one")
+		err = beeep.Beep(beeep.DefaultFreq, beeep.DefaultDuration)
+		beepTest()
 		localState.progressBar.sounds++
 	}
 	if localState.progressBar.progress > 1-p*200 && localState.progressBar.sounds == 1 {
 		fmt.Println("two")
+		err = beeep.Beep(beeep.DefaultFreq, beeep.DefaultDuration)
+		beepTest()
 		localState.progressBar.sounds++
 	}
 	if localState.progressBar.progress > 1-p*100 && localState.progressBar.sounds == 2 {
 		fmt.Println("three")
+		err = beeep.Beep(beeep.DefaultFreq, beeep.DefaultDuration)
+		beepTest()
 		localState.progressBar.sounds++
 	}
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func beepTest() {
+	freq := 587.0
+	duration := 500
+	kernel32, _ := syscall.LoadLibrary("kernel32.dll")
+	beep32, _ := syscall.GetProcAddress(kernel32, "Beep")
+
+	defer syscall.FreeLibrary(kernel32)
+
+	syscall.Syscall(uintptr(beep32), uintptr(2), uintptr(int(freq)), uintptr(duration), 0)
 }
